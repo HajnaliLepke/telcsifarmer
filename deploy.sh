a93df1ee86e888c88376f67f885edc0e2c20743e
HajnaliLepke <halkhod@gmail.com>
2024-06-22 20:09:25 +0200

    undo comment out of deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-# docker stop go_telcsifarmer
-# docker rm go_telcsifarmer
-# docker rmi go_telcsifarmer:multistage
+docker stop go_telcsifarmer
+docker rm go_telcsifarmer
+docker rmi go_telcsifarmer:multistage
 docker build -t go_telcsifarmer:multistage -f Dockerfile.multistage .
 docker run --publish 6969:6969 --name go_telcsifarmer go_telcsifarmer:multistage
