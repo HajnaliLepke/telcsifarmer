8513ce3dd84ee9c36eab57c057051693a2920aa7
HajnaliLepke <halkhod@gmail.com>
2024-06-22 20:06:30 +0200

    almost done with deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker stop $1
-docker rm $1
-docker rmi $1
-docker build -t $1:multistage -f Dockerfile.multistage .
-docker run --publish 6969:6969 $1
+# docker stop go_telcsifarmer
+# docker rm go_telcsifarmer
+# docker rmi go_telcsifarmer:multistage
+docker build -t go_telcsifarmer:multistage -f Dockerfile.multistage .
+docker run --publish 6969:6969 --name go_telcsifarmer go_telcsifarmer:multistage
