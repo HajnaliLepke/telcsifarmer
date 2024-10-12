85024b15110556f68841093079858baf8f5e2366
HajnaliLepke <halkhod@gmail.com>
2024-10-12 18:26:59 +0200

    New backend boolean for hidden checkbox

--- models.go
+++ models.go
@@ -2,8 +2,9 @@ package main
 
 type TelcsiFarmolas struct {
 	// ID     uint
-	MinPrice uint `form:"minprice" binding:"required"`
-	MaxPrice uint `form:"maxprice" binding:"required"`
+	MinPrice        uint     `form:"minprice" binding:"required"`
+	MaxPrice        uint     `form:"maxprice" binding:"required"`
 	ImportantPhones []string `form:"good_phones[]" binding:"-"`
-	NeutralPhones []string `form:"neutral_phones[]" binding:"required"`
+	IsPhone         string   `form:"isphone" binding:"-"`
+	NeutralPhones   []string `form:"neutral_phones[]" binding:"required"`
 }
